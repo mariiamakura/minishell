941bc3003f5ee3b78747a8499a63d03d75ae1105
Yann Cardona <yann@Yanns-MBP.fritz.box>
2023-07-24 23:43:28 +0200

    problems with the NULL-Termination of blocks

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mparasku <mparasku@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ycardona <ycardona@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 09:47:20 by ycardona          #+#    #+#             */
-/*   Updated: 2023/07/21 17:13:45 by mparasku         ###   ########.fr       */
+/*   Updated: 2023/07/24 23:33:57 by ycardona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,22 @@ int	main(void)
 	if (data == NULL)
 		return (1);
 	ft_parse(data);
+	
+	int i = 0;
+	int j = 0;
+	while (i < data->pipe_num + 1)
+	{
+		while (data->tokens[i][j])
+		{
+
+			printf("%s\n", data->tokens[i][j]);
+			j++;
+		}
+		//printf("null: %s\n", data->tokens[i][j]);
+		i++;
+	}
+	printf("%s\n", data->tokens[0][0]);
 	//print_tokens(data);
 	//printf("%d\n", execve(data->tokens[0][0], data->tokens[0], NULL));
-	start_pipes(data);
+	//start_pipes(data);
 }
