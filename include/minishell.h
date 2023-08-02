0345e4eb97aa23d0573e2c1119511198924bfd34
Mariia Paraskun <mparasku@c0c5c2.42berlin.de>
2023-08-02 16:43:03 +0200

    chmod for echo

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mparasku <mparasku@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 09:39:47 by ycardona          #+#    #+#             */
-/*   Updated: 2023/08/02 15:19:48 by mparasku         ###   ########.fr       */
+/*   Updated: 2023/08/02 16:39:21 by mparasku         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,5 +97,6 @@ void    ft_run_builtin(t_data *data, int i);
 int ft_echo(char *av[], int index, t_data *data);
 void ft_print(char *av[], int i, int flag, int index, t_data *data);
 void ft_cd(char *av[]/* , t_data *data */);
+int ft_prem_output(t_data *data, int index);
 
 #endif
--- src/buildins/ft_echo.c
+++ src/buildins/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: mparasku <mparasku@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 18:40:47 by mparasku          #+#    #+#             */
-/*   Updated: 2023/08/02 15:20:32 by mparasku         ###   ########.fr       */
+/*   Updated: 2023/08/02 16:41:24 by mparasku         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,32 @@
 
 int ft_echo(char *av[], int index, t_data *data)
 {
-	// if (ft_strncmp(av[0], "echo", ft_strlen("echo")) != 0)
-	// 	return (-1);
-	if (ft_strncmp(av[1], "-n", ft_strlen("-n")) == 0)	
+	if (av[1] == NULL) {
+		ft_putstr_fd("\n", data->pipes[index][1]);
+		return(0);
+	}
+	else if (ft_prem_output(data, index) == FALSE)
+	{
+		return (-1);
+	}
+	else if (ft_strncmp(av[1], "-n", ft_strlen("-n")) == 0)	
 		ft_print(av, 2, TRUE, index, data);
 	else 
 		ft_print(av, 1, FALSE, index, data);
 	return (0);
 	
+}
+
+int ft_prem_output(t_data *data, int index)
+{
+	struct stat file_info;
+	
+	if (fstat(data->pipes[index][1], &file_info) != 0)
+	{
+		ft_putstr_fd("minishell: fstat: Permission denied\n", STDERR_FILENO);
+		return (FALSE);
+	}
+	return (TRUE);
 }
 
 void ft_print(char *av[], int i, int flag, int index, t_data *data)
