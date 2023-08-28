fe4d628cd437799bf832965448e7fe0596f7fc31
Mariia Paraskun <mparasku@c0c7c3.42berlin.de>
2023-08-28 14:53:15 +0200

    echo -n added

--- src/buildins/ft_echo.c
+++ src/buildins/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: mparasku <mparasku@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 18:40:47 by mparasku          #+#    #+#             */
-/*   Updated: 2023/08/23 16:57:01 by mparasku         ###   ########.fr       */
+/*   Updated: 2023/08/28 14:51:41 by mparasku         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	ft_echo(char *av[], t_data *data, int index)
 
 int	ft_all_n(char *option)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (option[i] != '-')
